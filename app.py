83798f1ca1a68ec5c1bc93d6e8bacede8d6aff8e
Chan Feng <feng443@gmail.com>
2018-05-20 11:26:28 -0400

    Belley Button Biodiversity Dashboard working now

---
+++ app.py
@@ -0,0 +1,173 @@
+# import necessary libraries
+import numpy as np
+import pandas as pd
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.sql import select
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+# @TODO: Setup your database here
+#from flask_sqlalchemy import SQLAlchemy
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///DataSets/belly_button_biodiversity.sqlite"
+engine = create_engine("sqlite:///DataSets/belly_button_biodiversity.sqlite")
+# db = SQLAlchemy(app)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Otu = Base.classes.otu
+Samples = Base.classes.samples
+SampleMetadata = Base.classes.samples_metadata
+
+session = Session(engine)
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/names')
+def names():
+    """List of sample names.
+
+    Returns a list of sample names in the format
+    [
+        "BB_940",
+        "BB_941",
+        "BB_943",
+        "BB_944",
+        "BB_945",
+        "BB_946",
+        "BB_947",
+        ...
+    ]
+    """
+    df = pd.read_sql_query('select * from samples limit 1', engine)
+    return jsonify(df.columns[1:].tolist())
+
+#print(names())
+
+@app.route('/metadata/<sample>')
+def metadata(sample):
+    """MetaData for a given sample.
+
+    Args: Sample in the format: `BB_940`
+
+    Returns a json dictionary of sample metadata in the format
+
+    {
+        AGE: 24,
+        BBTYPE: "I",
+        ETHNICITY: "Caucasian",
+        GENDER: "F",
+        LOCATION: "Beaufort/NC",
+        SAMPLEID: 940
+    }
+    """
+    sample = sample.replace('BB_', '')
+    columns = ['AGE', 'BBTYPE', 'ETHNICITY', 'GENDER', 'LOCATION', 'SAMPLEID']
+    result = session.query(
+        SampleMetadata.AGE,
+        SampleMetadata.BBTYPE,
+        SampleMetadata.ETHNICITY,
+        SampleMetadata.GENDER,
+        SampleMetadata.LOCATION,
+        SampleMetadata.SAMPLEID
+    ).filter(SampleMetadata.SAMPLEID == sample).first()
+    if result:
+        return jsonify({x: result[i] for i, x in enumerate(columns)})
+    else:
+        return jsonify(None)
+
+#print(metadata('BB_940'))
+
+@app.route('/otu')
+def otu():
+    """List of OTU descriptions.
+
+    Returns a list of OTU descriptions in the following format
+
+    [
+        "Archaea;Euryarchaeota;Halobacteria;Halobacteriales;Halobacteriaceae;Halococcus",
+        "Archaea;Euryarchaeota;Halobacteria;Halobacteriales;Halobacteriaceae;Halococcus",
+        "Bacteria",
+        "Bacteria",
+        "Bacteria",
+        ...
+    ]
+    """
+    return jsonify([x[0] for x in session.query(Otu.lowest_taxonomic_unit_found).all()])
+
+@app.route('/wfreq/<sample>')
+def wfreq(sample):
+    """Weekly Washing Frequency as a number.
+
+    Args: Sample in the format: `BB_940`
+
+    Returns an integer value for the weekly washing frequency `WFREQ`
+    """
+    sample = sample.replace('BB_', '')
+    result = session.query(SampleMetadata.WFREQ).filter(SampleMetadata.SAMPLEID == sample).first()
+    if result:
+        return jsonify(result[0])
+    else:
+        return jsonify(None)
+
+#print(wfreq('BB_940'))
+
+@app.route('/samples/<sample>')
+def samples(sample):
+    """OTU IDs and Sample Values for a given sample.
+
+    Sort your Pandas DataFrame (OTU ID and Sample Value)
+    in Descending Order by Sample Value
+
+    Return a list of dictionaries containing sorted lists  for `otu_ids`
+    and `sample_values`
+
+    [
+        {
+            otu_ids: [
+                1166,
+                2858,
+                481,
+                ...
+            ],
+            sample_values: [
+                163,
+                126,
+                113,
+                ...
+            ]
+        }
+    ]
+    """
+    df = pd.read_sql_table('samples', engine)
+    df = df.loc[:, [sample]].query( sample + '> 0')
+    df = df.sort_values(sample, ascending=False)
+    return jsonify([{
+        'otu_ids': df.index.values.tolist(),
+        'sample_values': df[sample].tolist()
+    }])
+
+if __name__ == "__main__":
+    app.run(debug=True,
+            extra_files=['templates/index.html,'
+                         'static/js/app.js',
+                         'static/css/style.css'])
---
+++ static/js/app.js
@@ -0,0 +1,219 @@
+
+url = '/names';
+Plotly.d3.json(url, (error, data) => {
+   if (error) return console.log(data);
+   SELECT = document.getElementById("selDataset");
+   data.forEach( sample => SELECT.add( new Option(sample) ));
+});
+
+// Store otu_ids so it can be reused
+url = '/otu';
+OTU_DICT = {};
+Plotly.d3.json(url, (error, data) => {
+   if (error) return console.log(data);
+   data.forEach( (x, i) => OTU_DICT[i+1] = x );
+});
+
+sample = 'BB_940';
+buildPiePlot(sample, true); // TODO change to SELECT value
+buildBubblePlot(sample, true);
+buildMetadata(sample, true);
+buildGaugePlot(sample, true);
+
+// Init = True, newPlot, otherwise restyle
+// TODO: Fix Hover Text and lables.
+function buildPiePlot(sample, init) {
+    url="/samples/" + sample;
+
+    Plotly.d3.json(url, (error, data) => {
+        if (error) return console.log(data);
+        labels = data[0]['otu_ids'].slice(0,10);
+        values = data[0]['sample_values'].slice(0,10);
+        hoverText = [];
+        labels.forEach( l => hoverText.push(OTU_DICT[l]));
+        var pieData = [{
+            labels: labels,
+            values: values,
+            hovertext: hoverText,
+            type: 'pie'
+         }];
+
+         var layout = {
+            margin: {
+                t: 0,
+                l: 0,
+                r: 0,
+                b: 0,
+            },
+            //height: 400,
+            //width: 500,
+         }
+
+        //console.log(hoverText);
+        if (init) {
+            Plotly.newPlot('pie', pieData, layout);
+        } else {
+            //console.log('restyle pie');
+            Plotly.restyle('pie', 'values', [values]);
+            Plotly.restyle('pie', 'labels', [labels]);
+            Plotly.restyle('pie', 'hovertext', [hoverText]);
+        }
+
+    });
+}
+
+function optionChanged(sample) {
+    buildPiePlot(sample, false);
+    buildBubblePlot(sample, false);
+    buildMetadata(sample, false);
+    buildGaugePlot(sample,false);
+}
+
+function buildBubblePlot(sample, init) {
+    //console.log(sample);
+    url="/samples/" + sample;
+    //console.log(url);
+
+    Plotly.d3.json(url, (error, data) => {
+        if (error) return console.log(data);
+        x = data[0]['otu_ids'].slice(0,10);
+        y = data[0]['sample_values'].slice(0,10);
+        hoverText = [];
+        x.forEach( l => hoverText.push(OTU_DICT[l]));
+        var pieData = [{
+            x: x,
+            y: y,
+            mode: 'markers',
+            marker: {
+                size: y,
+                color: x,
+            },
+            hovertext: hoverText,
+            hoverinfo: 'all',
+            type: 'scatter'
+         }];
+
+         var layout = {
+            margin: {
+                t: 20,
+                l: 20,
+                r: 20,
+                b: 20,
+            },
+            height: 400,
+            hovermode: 'closest',
+         }
+
+        if (init) {
+            Plotly.newPlot('bubble', pieData, layout);
+        } else {
+            //console.log('restyle pie');
+            Plotly.restyle('bubble', 'x', [x]);
+            Plotly.restyle('bubble', 'y', [y]);
+            Plotly.restyle('bubble', 'hovertext', [hoverText]);
+        }
+    });
+}
+
+function buildMetadata(sample, init) {
+    Plotly.d3.json('/metadata/' + sample, (error, data) => {
+        if (error) return console.log(data);
+        var text = '';
+        for (var key in data)
+            text += key + ': ' + data[key] + '<br>';
+        document.getElementById('metadata').innerHTML = text;
+    });
+}
+
+function buildGaugePlot(sample, init) {
+    console.log('buildGaugePlot');
+    // Enter a speed between 0 and 180
+    var level; //  = 175;
+    url = 'wfreq/' + sample;
+
+    Plotly.d3.json(url, (error, data) => {
+        if (error) return console.log(data);
+        console.log('freq: ' + data);
+        level = data;
+        console.log(level);
+
+        // Trig to calc meter point
+        const MAX = 9;
+        const PART = 9;
+        const RADIUS = 0.5;
+        const FIFTY = 100;
+
+        var degrees = MAX - level;
+        var radians = degrees * Math.PI / MAX;
+        var x = RADIUS * Math.cos(radians);
+        var y = RADIUS * Math.sin(radians);
+
+        // Path: may have to change to create a better triangle
+        var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+        var data = [{
+            type: 'scatter',
+            x: [0],
+            y:[0],
+            marker: {size: 28, color:'850000'},
+            showlegend: false,
+            name: 'speed',
+            text: level,
+            hoverinfo: 'none',
+         }, {
+            values: [FIFTY/PART, FIFTY/PART, FIFTY/PART, FIFTY/PART, FIFTY/PART, FIFTY/PART, FIFTY/PART, FIFTY/PART, FIFTY/PART, FIFTY],
+            rotation: 90,
+            text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+            textinfo: 'text',
+            textposition:'inside',
+            marker: {colors:[
+                         'rgba(255, 80, 0, .5)', 'rgba(160, 80, 0, .5)', 'rgba(90, 80, 0, .5)',
+                         'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)', 'rgba(170, 202, 42, .5)',
+                          'rgba(202, 209, 95, .5)', 'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                         'rgba(255, 255, 255, 0)',
+                         ]},
+            //labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+            hoverinfo: 'none',
+            hole: .5,
+            type: 'pie',
+            showlegend: false
+        }];
+
+        var layout = {
+            shapes:[{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                }
+            }],
+            title: 'Gauge Speed 0-100',
+            margin: {
+                t: 50,
+                l: 20,
+                r: 20,
+                b: 20,
+            },
+            //height: 400,
+            //width: 500,
+            xaxis: {
+                zeroline:false,
+                showticklabels:false,
+                showgrid: false,
+                range: [-1, 1]},
+            yaxis: {
+                zeroline:false,
+                showticklabels:false,
+                showgrid: false,
+                range: [-1, 1]}
+        };
+
+        Plotly.newPlot('gauge', data, layout);
+    });
+}
